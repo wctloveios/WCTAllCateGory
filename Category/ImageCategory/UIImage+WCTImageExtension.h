976cf2ec78c8b5aade6f61a12731a29affc8cb05
wct <wctloveios@163.com>
2018-05-22 20:52:10 +0800

    增加颜色生成图片+图片不拉伸显示方法

---
+++ Category/ImageCategory/UIImage+WCTImageExtension.h
@@ -0,0 +1,45 @@
+//
+//  UIImage+WCTImageExtension.h
+//  WCTAllFunctionText
+//
+//  Created by mac on 2018/5/22.
+//  Copyright © 2018年 王春涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIImage (WCTImageExtension)
+
+/**
+ 颜色生成图片
+ 
+ @param color 颜色
+ @return 图片
+ */
++ (nullable UIImage *)WCT_imageWithColor:(UIColor *)color;
+
+/**
+ 颜色生成图片
+ 
+ @param color 颜色
+ @param size 图片显示尺寸
+ @param cornerRadius 图片圆角
+ @return 图片
+ */
++ (nullable UIImage *)WCT_imageWithColor:(UIColor *)color size:(CGSize)size cornerRadius:(CGFloat)cornerRadius;
+
+/**
+ 图片不拉伸
+ 
+ @param image 图片
+ @param size 图片区域
+ @return 图片
+ */
++ (UIImage *)WCT_imageCompress:(UIImage *)image toSize:(CGSize)size;
+
+@end
+
+NS_ASSUME_NONNULL_END
+
---
+++ Category/ImageCategory/UIImage+WCTImageExtension.m
@@ -0,0 +1,86 @@
+//
+//  UIImage+WCTImageExtension.m
+//  WCTAllFunctionText
+//
+//  Created by mac on 2018/5/22.
+//  Copyright © 2018年 王春涛. All rights reserved.
+//
+
+#import "UIImage+WCTImageExtension.h"
+
+@implementation UIImage (WCTImageExtension)
+
++ (nullable UIImage *)WCT_imageWithColor:(UIColor *)color {
+    CGRect rect = CGRectMake(0.0f, 0.0f, 1.0f, 1.0f);
+    UIGraphicsBeginImageContext(rect.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetFillColorWithColor(context, [color CGColor]);
+    CGContextFillRect(context, rect);
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return image;
+}
+
++ (nullable UIImage *)WCT_imageWithColor:(UIColor *)color size:(CGSize)size cornerRadius:(CGFloat)cornerRadius {
+    UIImage *resultImage = nil;
+    UIGraphicsBeginImageContextWithOptions(size, NO, 0);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetFillColorWithColor(context, color.CGColor);
+    
+    if (cornerRadius > 0) {
+        UIBezierPath *path = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(0, 0, size.width, size.height) cornerRadius:cornerRadius];
+        [path addClip];
+        [path fill];
+    }else{
+        CGContextFillRect(context, CGRectMake(0, 0, size.width, size.height));
+    }
+    resultImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return resultImage;
+}
+
++ (UIImage *)WCT_imageCompress:(UIImage *)image toSize:(CGSize)size {
+    UIImage *newImage = nil;
+    CGSize imageSize = image.size;
+    CGFloat width = imageSize.width;
+    CGFloat height = imageSize.height;
+    CGFloat targetWidth = size.width;
+    CGFloat targetHeight = size.height;
+    CGFloat scaleFactor = 0.0;
+    CGFloat scaledWidth = targetWidth;
+    CGFloat scaledHeight = targetHeight;
+    CGPoint thumbnailPoint = CGPointMake(0.0, 0.0);
+    if(CGSizeEqualToSize(imageSize, size) == NO){
+        CGFloat widthFactor = targetWidth / width;
+        CGFloat heightFactor = targetHeight / height;
+        
+        if(widthFactor > heightFactor){
+            scaleFactor = widthFactor;
+        }else{
+            scaleFactor = heightFactor;
+        }
+        scaledWidth = width * scaleFactor;
+        scaledHeight = height * scaleFactor;
+        
+        if(widthFactor > heightFactor){
+            thumbnailPoint.y = (targetHeight - scaledHeight) *0.5;
+        }else if(widthFactor < heightFactor){
+            thumbnailPoint.x = (targetWidth - scaledWidth) *0.5;
+        }
+    }
+    UIGraphicsBeginImageContext(size);
+    CGRect thumbnailRect = CGRectZero;
+    thumbnailRect.origin = thumbnailPoint;
+    thumbnailRect.size.width = scaledWidth;
+    thumbnailRect.size.height = scaledHeight;
+    [image drawInRect:thumbnailRect];
+    newImage = UIGraphicsGetImageFromCurrentImageContext();
+    if(newImage == nil){
+        NSLog(@"scale image fail");
+    }
+    UIGraphicsEndImageContext();
+    return newImage;
+}
+
+
+@end
