068362c8c008fad73ae6c12b14546cd8e5e8f2ec
wct <wctloveios@163.com>
2018-05-23 21:59:40 +0800

    新增头文件管理分类文件

---
+++ WCTAllCategory/WCTAllCategory.h
@@ -0,0 +1,24 @@
+//
+//  WCTAllCategory.h
+//  WCTAllCategory
+//
+//  Created by mac on 2018/5/23.
+//  Copyright © 2018年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WCTAllCategory.
+FOUNDATION_EXPORT double WCTAllCategoryVersionNumber;
+
+//! Project version string for WCTAllCategory.
+FOUNDATION_EXPORT const unsigned char WCTAllCategoryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WCTAllCategory/PublicHeader.h>
+
+#import <WCTAllCategory/UIButton+WCTButtonExtension.h>
+#import <WCTAllCategory/UIColor+WCTColorExtension.h>
+#import <WCTAllCategory/UIFont+WCTFontExtension.h>
+#import <WCTAllCategory/UIImage+WCTImageExtension.h>
+
+#import <WCTAllCategory/WCTSegmentedControl.h>
